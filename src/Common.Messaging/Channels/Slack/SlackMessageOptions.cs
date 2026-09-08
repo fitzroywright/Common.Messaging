94cef01a9b38411384a485572684734cd0f47998
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-08 07:48:30 -0500

    Move Slack adapter into Common.Messaging channel folder

--- src/Common.Messaging/Channels/Slack/SlackMessageOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Common.Messaging.Channels.Slack;
-
-public sealed class SlackMessageOptions
-{
-    public string? BotToken { get; set; }
-
-    public string? DefaultChannel { get; set; }
-}
