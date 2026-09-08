56cd906d1c1cc48c79be79b31165994c437f03de
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-08 07:47:06 -0500

    Add Slack channel options

---
+++ src/Common.Messaging/Channels/Slack/SlackMessageOptions.cs
@@ -0,0 +1,8 @@
+namespace Common.Messaging.Channels.Slack;
+
+public sealed class SlackMessageOptions
+{
+    public string? BotToken { get; set; }
+
+    public string? DefaultChannel { get; set; }
+}
